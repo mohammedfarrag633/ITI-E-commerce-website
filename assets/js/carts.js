2702caf072d74eb2b47a62fd208d25783355c184
abdallahmmu <abdallahiifox@gmail.com>
2023-04-08 19:38:52 +0200

    add:added the functionality to append the cart item and calculate the Total amount

--- assets/js/carts.js
+++ assets/js/carts.js
@@ -3,9 +3,9 @@ import { getLocalStorageForBadge } from "./helpers/getLocalStorageForBadge.js";
 //get all elements
 const noCartDiv = document.getElementById("no-cart");
 const cartItemDiv = document.getElementById("cart-item");
-
+const cartItemRow = document.getElementById("cart-item-row");
 window.addEventListener("load", (e) => {
-  const localStorageCart = localStorage.getItem("myCart");
+  const localStorageCart = JSON.parse(localStorage.getItem("myCart")); // converting the JSON to Object
 
   //checking the local Storage
 
@@ -15,6 +15,59 @@ window.addEventListener("load", (e) => {
   } else {
     noCartDiv.style.display = "none";
     cartItemDiv.style.display = "block";
+
+    appendCartItems(localStorageCart);
   }
 });
+
+//append Cart Item
+function appendCartItems(cartItems) {
+  cartItems.forEach((item, i) => {
+    const cardDivLeft = document.createElement("div");
+    cardDivLeft.classList.add("col-12", "col-sm-6");
+    const cardDivReight = document.createElement("div");
+    cardDivReight.classList.add("col-12", "col-sm-6");
+
+    cardDivLeft.innerHTML += `
+
+    <div class="image-product d-flex align-items-center justify-content-center justify-content-sm-start">
+
+    <img src="${item.image}" alt="${item.title}" class="w-50 object-fit-cover">
+</div>
+    `;
+
+    cardDivReight.innerHTML += `
+    <div class="card-body mt-5">
+    <h5 class="card-title fs-4 fw-bold">${item.title}</h5>
+    <p class="card-text mt-2">${item.description}</p>
+    <p class="card-text"><small class="text-body-secondary">Price : EGP ${item.price}</small></p>
+    <div class="d-flex align-items-center">
+
+        <button class="btn bg-dark text-white m-3" onclick="addToCart(${item.id})"> +</button>
+        <span class="bg-dark text-white p-1 rounded" id="qty">QTY: ${item.qty}</span>
+        <button class="btn bg-dark text-white m-3">-</button>
+    </div>
+  </div>
+  `;
+    cartItemRow.appendChild(cardDivLeft);
+    cartItemRow.appendChild(cardDivReight);
+  });
+}
+
+//get total amount
+function totalAmount() {
+  const totalDiv = document.getElementById("total");
+
+  const getLoclMyCart = JSON.parse(localStorage.getItem("myCart"));
+
+  if (getLoclMyCart) {
+    const getTotal = getLoclMyCart.reduce((acc, prev) => {
+      acc += prev.price * prev.qty;
+      return acc;
+    }, 0);
+
+    totalDiv.innerHTML = getTotal;
+  }
+}
+totalAmount();
 getLocalStorageForBadge();
